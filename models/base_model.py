feed4a54dddcdeada9822eff7650bd9430784e5d
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-12-01 21:07:44 -0400

    Update task 10

--- models/base_model.py
+++ models/base_model.py
@@ -21,19 +21,14 @@ class BaseModel:
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            if '__class__' in kwargs:
-                del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
--- models/state.py
+++ models/state.py
@@ -11,24 +11,22 @@ from models.city import City
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade="all, delete")
-    else:
 
-        name = ""
-        # @property
-        # def cities(self):
-        #     """Getter attribute cities that
-        # returns the list of City instances"""
-        #     city_list = []
-        #     all_cities = models.storage.all(City)
-        #     for city in all_cities.values():
-        #         if city.state_id == self.id:
-        #             city_list.append(city)
-        #     return city_list
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete")
+    # @property
+    # def cities(self):
+    #     """Getter attribute cities that
+    # returns the list of City instances"""
+    #     city_list = []
+    #     all_cities = models.storage.all(City)
+    #     for city in all_cities.values():
+    #         if city.state_id == self.id:
+    #             city_list.append(city)
+    #     return city_list
 
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
             """Returns the list of City instances with
