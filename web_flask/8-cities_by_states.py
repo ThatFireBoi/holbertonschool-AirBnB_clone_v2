1f17c4c8763180116555a809d01de5a9ed685abc
ThatFireBoi <gcf2007@hotmail.com>
2023-12-14 08:47:42 -0400

    Fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,23 +4,22 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    state_li = storage.all(State).values()
-    return render_template('7-states_list.html', states=state_li)
-
-
-@app.route('cities_by_states', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    city_li = storage.all(State).values()
-    return render_template('8-cities_by_states.html', cities=city_li)
+    """Display a HTML page with a list of states"""
+    states = storage.all(State)
+    cities = storage.all(City)
+    return render_template('8-cities_by_states.html', states=states,
+                           cities=cities)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def teardown_db(exception):
+    """Close storage"""
     storage.close()
 
 
