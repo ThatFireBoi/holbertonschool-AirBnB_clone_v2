df0dca7fad434e92a7f64d91ca5e2d9027eee140
ThatFireBoi <gcf2007@hotmail.com>
2023-12-15 15:56:44 -0400

    Quotation correction

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,14 +3,14 @@
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
+
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Display a HTML page with a list of states"""
-    states = storage.all(State)
+    states = storage.all('State')
     return render_template('7-states_list.html', states=states)
 
 
