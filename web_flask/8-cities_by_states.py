5023a5494b9575f5bcc5ab46be8939227ac63013
ThatFireBoi <gcf2007@hotmail.com>
2023-12-15 19:10:58 -0400

    Changes to headers and cities method

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,11 +8,18 @@ from models.city import City
 app = Flask(__name__)
 
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a HTML page with a list of states"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """Display a HTML page with a list of states"""
-    states = storage.all(State)
-    cities = storage.all(City)
+    states = storage.all(State).values()
+    cities = storage.all(City).values()
     return render_template('8-cities_by_states.html', states=states,
                            cities=cities)
 
