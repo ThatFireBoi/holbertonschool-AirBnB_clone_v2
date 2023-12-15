118b9a0e1ceb7c9b9e77c55748d53c04fa440acf
ThatFireBoi <gcf2007@hotmail.com>
2023-12-15 16:03:58 -0400

    correction to import

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -54,7 +54,7 @@ def number_odd_or_even(n):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Display a HTML page with a list of states"""
-    states = storage.all('State')
+    states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
