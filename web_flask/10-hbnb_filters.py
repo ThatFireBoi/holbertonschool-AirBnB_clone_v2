52f0966e8e9c885024d25b45d4925d8371e2fe36
ThatFireBoi <gcf2007@hotmail.com>
2023-12-15 22:17:31 -0400

    Fixes

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
+"""Starts a Flask web application.
 
-from flask import Flask, render_template
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /hbnb_filters: HBnB HTML filters page.
+"""
 from models import storage
-from models.state import State
-from models.amenity import Amenity
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
+@app.route("/hbnb_filters", strict_slashes=False)
 def hbnb_filters():
-    """Display a HTML page with a list of states"""
-    states = storage.all(State).values()
-    amenity = storage.all(Amenity).values()
-    return render_template('10-hbnb_filters.html', states=states,
-                           amenity=amenity)
+    """Displays the main HBnB filters HTML page."""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Close storage"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
